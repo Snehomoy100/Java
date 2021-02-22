cdc864b315c04ee2b47eea3339643a99fbadff96
Snehomoy Maitra <snehomoy100@gmail.com>
2021-02-23 01:13:54 +0530

    first pattern printing

--- ForLoop.java
+++ ForLoop.java
@@ -1,7 +1,14 @@
 public class ForLoop {
+
     public static void main(String[] args) {
+
+
         for(int counter = 1; counter <= 5; counter++) {
+
+            // for(initilization; condition; re-initialization)
+
             System.out.println("Hey, I am learning For Loop in Java");
+            
         }
     }
 }
---
+++ Patterns/Pattern1.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        for(int rows = 1; rows <= n; rows++) {
+            System.out.println("*");
+        }
+    }
+}

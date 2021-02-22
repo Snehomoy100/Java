81fd8347096e14deefe8fdd147d90b8c4639920f
Snehomoy Maitra <snehomoy100@gmail.com>
2021-02-23 00:23:23 +0530

    reverse of any given no.

---
+++ Reverse_of_no.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Reverse_of_no {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        System.out.println(n);
+    }
+}

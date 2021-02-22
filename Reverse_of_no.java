ac0ce64c8061d5f141f6397a7441d1c4d1cc7c84
Snehomoy Maitra <snehomoy100@gmail.com>
2021-02-23 00:34:36 +0530

    updated reverse of any given no.

--- Reverse_of_no.java
+++ Reverse_of_no.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Rem;
+
 public class Reverse_of_no {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        int ans = 0;
 
-        System.out.println(n);
+        while(n != 0){
+            int rem = n % 10;
+            ans = (ans * 10) + rem;
+            n = n/10;
+        }
+        System.out.println(ans);
     }
 }

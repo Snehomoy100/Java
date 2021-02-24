b2931f1eea185c9f057cd6952e273ba62b8380e1
Snehomoy Maitra <snehomoy100@gmail.com>
2021-02-25 02:20:38 +0530

    pattern6 file added

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int nst = 1;
+
+        for(int row = 1; row <= n; row++) {
+
+            for(int cst = 1; cst <= nst; cst++) {
+                System.out.println("*");
+            }
+
+            if () {
+                
+            } else {
+                
+            }
+            System.out.println();
+        }
+    }
+}

c947e7c05cf128381f9a405bfb378557be7e216e
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-06 00:07:23 +0530

    just updated

--- CeltoFer.java
+++ CeltoFer.java
@@ -1,5 +1,16 @@
 public class CeltoFer {
     public static void main(String[] args) {
+
+        int fmin = 0;
+        int fmax = 300;
+        int fcurrent = fmin;
+        int fstep = 20;
         
+        while(fcurrent<=fmax){
+
+            int celsius = (int)((5.0/9)*(fcurrent - 32));
+            System.out.println(fcurrent + "\t" + celsius);
+            fcurrent = fcurrent + fstep;
+        }
     }
 }

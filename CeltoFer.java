b2577f3a3066bc67192e15f5bbda790933c06656
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-05 19:39:33 +0530

    Celcius to ferhenite code added

---
+++ CeltoFer.java
@@ -0,0 +1,5 @@
+public class CeltoFer {
+    public static void main(String[] args) {
+        
+    }
+}
--- Reverse_of_no.java
+++ Reverse_of_no.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Reverse_of_no {
     public static void main(String[] args) {
         

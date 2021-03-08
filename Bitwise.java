3a8195ff817fcbc509b84fc4303ae1335ed14032
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-08 21:17:59 +0530

    bitwise updated

--- Bitwise.java
+++ Bitwise.java
@@ -4,5 +4,17 @@ public class Bitwise {
         System.out.println(2 | 3);
         System.out.println(2 ^ 3);
         System.out.println(~2);
+
+
+        
+        // >> = left shift
+        // << = right shift
+
+        System.out.println(5 >> 1);
+        System.out.println(34 >> 2);
+        System.out.println(5 << 3);
+        System.out.println(42 << 10);
+        System.out.println(-8 << 3);
+        System.out.println(-64 >> 4);
     }
 }
---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo {
+    public static void main(String args[])
+       {
+           int a = 42;
+           int b = ~a; 
+           // (there is tilde sign before a)
+           
+           System.out.print(a + " " + b);     	
+       }
+}

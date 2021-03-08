9b342de03466fb9dcae8638cf29258a8e9393f6d
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-09 01:52:07 +0530

    demo updated

--- demo.java
+++ demo.java
@@ -1,10 +1,17 @@
 public class demo {
     public static void main(String args[])
        {
-           int a = 42;
-           int b = ~a; 
-           // (there is tilde sign before a)
-           
-           System.out.print(a + " " + b);     	
+        
+        // Guess the output 
+
+            int x = 10;
+            int y = 20;
+            if(x++ > 10 || ++y > 20 ){
+        System.out.print("Inside if  ");
+            }else{
+        System.out.print("Inside else  ");
+            }
+
+            System.out.println(x  + " " + y);  	
        }
 }

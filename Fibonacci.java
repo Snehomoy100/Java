5a40ed1f456b2645fbe794ff52a16b79c4e3c27c
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-10 20:13:09 +0530

    Fibonacci updated & rough file added

--- Fibonacci.java
+++ Fibonacci.java
@@ -18,6 +18,6 @@ public class Fibonacci {
             counter++;
         }
 
-        
+        input.close();        
     }
 }
---
+++ rough.java
@@ -0,0 +1,11 @@
+public class rough {
+    public static void main(String[] args) {
+        for(int i = 0; i < 2; i = i + 1) {
+            for(int j = 0; j < 2; j = j + 1) {
+                 if (j == 1)
+                     break;
+                 System.out.print(j +" ");
+             }
+        } 
+    }
+}

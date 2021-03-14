9c0756f49bdc25829efe7b75805bddc25e28ad62
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-14 23:45:39 +0530

    updated

--- Example.java
+++ Example.java
@@ -2,5 +2,7 @@ public class Example {
     public static void main(String[] args) {
         int n = 1;
         System.out.print(n);
+        int p = p + 1;
+        System.out.println(p);
     }
 }

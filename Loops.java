f62c46998ec9c00fc7f5cb5987a45360a836a47a
Snehomoy Maitra <snehomoy100@gmail.com>
2021-03-16 00:45:03 +0530

    updated

---
+++ Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+		int x = 15;
+		if(x <= 15){
+			System.out.print("Inside if  ");
+		}else if(x == 15){
+			System.out.print("Inside else if  ");
+		}
+		System.out.println(x);
+    }
+}
